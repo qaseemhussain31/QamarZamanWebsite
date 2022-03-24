52fc44772686b80de9c05d5602ce1047a7681cbd
qaseemhussain31 <85561493+qaseemhussain31@users.noreply.github.com>
2022-03-24 11:06:47 +0500

    Created Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { IssuesComponent } from './issues/issues.component';
+import { EventComponent } from './event/event.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MultimediaComponent } from './multimedia/multimedia.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    IssuesComponent,
+    EventComponent,
+    GalleryComponent,
+    MultimediaComponent,
+    BlogComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
